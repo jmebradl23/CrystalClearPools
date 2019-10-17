82afdf9f8c8fe71071684da0408442ae5e819a69
hintonbradley <hintonbradley@gmail.com>
2019-10-16 22:24:17 -0700

    updated images

--- src/Components/Gallery/GalleryPage/Gallery.js
+++ src/Components/Gallery/GalleryPage/Gallery.js
@@ -1,30 +1,117 @@
 import React from 'react';
 import './Gallery.css';
 import ImageBox from '../ImageBox/ImageBox';
-import Img1 from '../../../img/img1.jpg';
-import Img2 from '../../../img/img2.jpg';
-import Img3 from '../../../img/img3.jpg';
-import Img4 from '../../../img/img4.jpg';
+import Img1 from '../../../img/gallery/001.jpg';
+import Img2 from '../../../img/gallery/002.jpg';
+import Img3 from '../../../img/gallery/003tall.jpg';
+import Img4 from '../../../img/gallery/004.jpg';
+import Img5 from '../../../img/gallery/005.jpg';
+import Img6 from '../../../img/gallery/006.jpg';
+import Img7 from '../../../img/gallery/007.jpg';
+import Img8 from '../../../img/gallery/008tall.jpg';
+import Img9 from '../../../img/gallery/009.jpg';
+import Img10 from '../../../img/gallery/010.jpg';
+import Img11 from '../../../img/gallery/011.jpg';
+import Img12 from '../../../img/gallery/012.jpg';
+import Img13 from '../../../img/gallery/013.jpg';
+// import Img14 from '../../../img/gallery/014.jpg';
+import Img15 from '../../../img/gallery/015.jpg';
+import Img16 from '../../../img/gallery/016.jpg';
+import Img17 from '../../../img/gallery/017.jpg';
+import Img18 from '../../../img/gallery/018.jpg';
+import Img19 from '../../../img/gallery/019.jpg';
+import Img20 from '../../../img/gallery/020.jpg';
 
 
 class Gallery extends React.Component {
     constructor(props) {
         super(props);
+        this.imgContainer = React.createRef()
+        this.myTest = this.myTest.bind(this);
+        // this.myTest.bind(this);
     }
 
-    state = {}
+    state = {
+        id: null,
+        imgBoxes: null
+    }
 
-    componentDidMount () {}
+    componentDidMount () {
+        // console.log(this.imgContainer.current.childNodes);
+        this.setState({imgBoxes: this.imgContainer.current.childNodes})
+    }
+
+    removeFade() {
+        for(let i=0; i<this.state.imgBoxes.length; i++) {
+            if (!(this.state.imgBoxes[i].classList.contains('expand'))) {
+                this.state.imgBoxes[i].classList.remove('fade');
+            }
+        }
+    }
+
+    addFade () {
+        for(let i=0; i<this.state.imgBoxes.length; i++) {
+            if (!(this.state.imgBoxes[i].classList.contains('expand'))) {
+                this.state.imgBoxes[i].classList.add('fade');
+            } else {
+                this.state.imgBoxes[i].classList.remove('fade');
+            }
+        }
+    }
+
+    myTest (e) {
+        // console.log(e.currentTarget.id)
+        if (e.currentTarget.id === this.state.id) {
+            e.currentTarget.classList.remove('expand')
+            this.setState({id: null})
+            this.removeFade();
+        } else {
+            // console.log('different');
+            e.currentTarget.classList.add('expand')
+            this.setState({id: e.currentTarget.id})
+            for(let i=0; i<this.state.imgBoxes.length; i++) {
+                if( (e.currentTarget.id != this.state.imgBoxes[i].id) && (this.state.imgBoxes[i].classList.contains('expand'))) {
+                    this.state.imgBoxes[i].classList.remove('expand')
+                }
+            }
+            this.addFade()
+        }
+        return false;
+
+        console.log(e.currentTarget.id);
+        for(let i=0; i<this.state.imgBoxes.length; i++) {
+            console.log(this.state.imgBoxes[i].id);
+            if(this.state.imgBoxes[i].id==='third') {
+                this.state.imgBoxes[i].classList.add("mystyle");
+            }
+        }
+    }
 
     render () {
         return (
             <section className="Gallery padding-container">
                 <h3 className="txt-ctr">Click on a photo for full view</h3>
-                <div className="full-container">
-                    < ImageBox id="first" img={Img1} />
-                    < ImageBox id="second" img={Img2} />
-                    < ImageBox id="third" img={Img3} />
-                    < ImageBox id="fourth" img={Img4} />
+                <div className={ this.state.id ? "grow full-container" : "full-container" } ref={this.imgContainer}>
+                    < ImageBox id="first" img={Img1} meth={this.myTest}/>
+                    < ImageBox id="second" img={Img2} meth={this.myTest}/>
+                    < ImageBox id="third" img={Img3} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img4} meth={this.myTest}/>
+                    < ImageBox id="first" img={Img5} meth={this.myTest}/>
+                    < ImageBox id="second" img={Img6} meth={this.myTest}/>
+                    < ImageBox id="third" img={Img7} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img8} meth={this.myTest}/>
+                    < ImageBox id="first" img={Img9} meth={this.myTest}/>
+                    < ImageBox id="second" img={Img10} meth={this.myTest}/>
+                    < ImageBox id="third" img={Img11} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img12} meth={this.myTest}/>
+                    < ImageBox id="first" img={Img13} meth={this.myTest}/>
+                    {/* < ImageBox id="second" img={Img14} meth={this.myTest}/> */}
+                    < ImageBox id="third" img={Img15} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img16} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img17} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img18} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img19} meth={this.myTest}/>
+                    < ImageBox id="fourth" img={Img20} meth={this.myTest}/>
                 </div>
             </section>
         );
--- src/Components/Gallery/ImageBox/ImageBox.js
+++ src/Components/Gallery/ImageBox/ImageBox.js
@@ -3,16 +3,28 @@ import React from 'react';
 class ImageBox extends React.Component {
     constructor(props) {
         super(props);
+        this.resizeHandler = this.resizeHandler.bind(this);
     }
 
-    state = {}
+    state = {
+        id: null,
+        expand: false,
+        fade: false
+    }
 
     componentDidMount () {}
 
     resizeHandler (e) {
+        console.log(this.state.id, e.currentTarget.id)
+        if(this.state.id===e.currentTarget.id) {
+            this.setState({expand: false, id: null});
+        } else {
+            this.setState({expand: true, id: e.currentTarget.id});
+        }
         // console.log(e.currentTarget.id)
-        let el = e.currentTarget.id;
-        el.addClass('expand');
+        // let el = e.currentTarget.id;
+        // console.log(this.props.id);
+        // console.log(this.state.id)
         // console.log(el[0].id)
         // $('.expand').each(function (index, ex) {
         //     if(ex.id!=el[0].id) {
@@ -30,13 +42,11 @@ class ImageBox extends React.Component {
         // }
     };
 
-
     render () {
         return (
-            <div id={this.props.id} className="img-container" onClick={this.resizeHandler}>
+            <div id={this.props.id} className={ this.state.expand ? "expand img-container" : "img-container" } onClick={this.props.meth}>
                 <p className="close">x</p>
                 <img src={this.props.img} alt="" />
-                <h1>This is a test</h1>
             </div>
         );
     }
