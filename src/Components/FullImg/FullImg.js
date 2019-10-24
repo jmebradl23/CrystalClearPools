a68827f1a4692bd135bb74f10f13b4433379fd9d
hintonbradley <hintonbradley@gmail.com>
2019-10-24 08:56:01 -0700

    cleaned up Home styling

--- src/Components/FullImg/FullImg.js
+++ src/Components/FullImg/FullImg.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import './FullImg.css';
+
 class FullImg extends React.Component {
 // function FullImg () {
 
     render () {
         return (
-            <section>
-                <section className={`sec-img sec${this.props.sec}`} >
-                    <h1 className={this.props.text ? 'txt-ctr txt-overlay': 'hidden'} >{this.props.text}</h1>
-                </section>
+            <section className={`FullImg sec-img sec${this.props.sec}`} >
+                <h1 className={this.props.text ? 'txt-ctr txt-overlay': 'hidden'} >{this.props.text}</h1>
             </section>
           )
     }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -28,7 +28,7 @@ class Home extends React.Component {
                             
                             <FontAwesomeIcon icon={faBriefcase} style={{color:"#000", margin: "0 auto", display: "block"}} size="3x" />
                             
-                            <hr></hr>
+                            <hr className="hr-25"></hr>
 
                             < Column title="Efficient" text="Crystal Clear Pool & Spa Service is the company of choice for pool owners in the greater Napa area who are looking for a professional, qualified and affordable pool maintenance team. Our years of training and experience not only make us knowledgeable, they also make us the very best company in Napa to handle most any pool maintenance and repair service quickly and efficiently." />
 
@@ -37,7 +37,7 @@ class Home extends React.Component {
                             
                             <FontAwesomeIcon icon={faSwimmer} style={{color:"#000", margin: "0 auto", display: "block"}} size="3x" />
                             
-                            <hr></hr>
+                            <hr className="hr-25"></hr>
 
                             < Column title="Dedicated" text="We have been proudly offering pool maintenance and repair services to Napa residents for many years. Being pool enthusiasts ourselves, we understand the commitment as well as the investment you have made when you became a Napa pool owner. We know how much work needs to be put in and around your pool to keep it clean, safe and enjoyable, and we also understand that, unlike us, caring for your Napa pool shouldn't be your primary occupation. Why spend more time on pool maintenance than on actually enjoying your pool?" />
 
@@ -46,7 +46,7 @@ class Home extends React.Component {
                             
                             <FontAwesomeIcon icon={faClock} style={{color:"#000", margin: "0 auto", display: "block"}} size="3x" />
 
-                            <hr></hr>
+                            <hr className="hr-25"></hr>
 
                             < Column title="Experienced" text="Crystal Clear Pool & Spa Service will be happy to take on part or all of your Napa pool maintenance, repair and renovation duties, allowing you more time to make the most of your investment in your free time. Crystal Clear Pool & Spa Service offers regular maintenance services, such as weekly Napa pool cleaning and chlorine monitoring as well as seasonal and unscheduled maintenance and repair services, such as pool painting and tile repair. Whatever your needs or your budget, we can help." />
                         
