28ff3a71fd45fd59630b43cd8cda9ccaeca35e06
hintonbradley <hintonbradley@gmail.com>
2020-09-23 18:16:06 -0700

    updated components, added content

--- src/Components/Column/Column.js
+++ src/Components/Column/Column.js
@@ -6,7 +6,7 @@ function Column (props) {
         return (
             <div>
                 <h4 className="txt-ctr">{props.title}</h4>
-                <p className="txt-ctr">{props.text}</p>
+                <p className="txt-justify">{props.text}</p>
             </div>
         )
 }
---
+++ src/content/aboutUsPage.js
@@ -0,0 +1,17 @@
+import Img1 from '../img/img1.jpg';
+import Img4 from '../img/img4.jpg';
+
+const aboutUsPage = [
+    {
+      description: 'Crystal Clear Pool & Spa Service has been offering quality pool maintenance and pool repair services for many years. While large enough to serve the greater St. Helena area, we are proud to offer personalized and attentive services to each and every one of our customers. At Crystal Clear Pool & Spa Service, we believe in treating our esteemed clients with the level of respect and care they deserve, regardless of the size of their project – or budget.',
+      image: Img1,
+      id: 'n984jgo'
+    },
+    {
+      description: 'Our team is comprised of highly trained and experienced pool technicians, ensuring that each and every maintenance and repair project is carried out quickly, efficiently and professionally. Our crew is courteous and respectful of your environment and privacy, which is why we strive to stay out of your way as much as possible and always try to accommodate the schedule that works best for you. Our goal is to work as hard and as quickly as possible to allow you to enjoy your swimming pool as much and as often as you like.',
+      image: Img4,
+      id: 'kj3ljf'
+    }
+  ];
+
+  export default aboutUsPage;
